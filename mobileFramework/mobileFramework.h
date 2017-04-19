6866071963221f75129ebf6aa9266a6ac16ea843
Peter Alt <peter.alt@philamuseum.org>
2017-04-19 16:00:48 -0400

    Initial Commit

---
+++ mobileFramework/mobileFramework.h
@@ -0,0 +1,19 @@
+//
+//  mobileFramework.h
+//  mobileFramework
+//
+//  Created by Peter.Alt on 4/19/17.
+//  Copyright © 2017 Philadelphia Museum of Art. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mobileFramework.
+FOUNDATION_EXPORT double mobileFrameworkVersionNumber;
+
+//! Project version string for mobileFramework.
+FOUNDATION_EXPORT const unsigned char mobileFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mobileFramework/PublicHeader.h>
+
+
