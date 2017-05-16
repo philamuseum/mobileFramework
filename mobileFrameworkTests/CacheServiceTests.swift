755166bb8fa30633a983272729b9c13dba7c5f4a
Peter Alt <peter.alt@philamuseum.org>
2017-05-16 15:27:55 -0400

    additional test to ensure updated local cache file will be served

--- mobileFrameworkTests/CacheServiceTests.swift
+++ mobileFrameworkTests/CacheServiceTests.swift
@@ -53,7 +53,7 @@ class CacheServiceTests: XCTestCase {
         }
     }
         
-    func test_load_single_file_cached() {
+    func test_load_single_file_cached_with_file_not_available_locally() {
         
         let expectationSuccess = expectation(description: "Waiting for caching service to process request.")
         
@@ -61,7 +61,7 @@ class CacheServiceTests: XCTestCase {
         
         let url = URL(string: "http://localhost/api/v0/collection/objectIsOnView?query=4&api_token=WtPr21h94VqPLxEo8079vzg1ZcE6sIwYYcXur80EOULRB79zO0WBqdERk5hG")
         
-        let sampleJSON = "{\"onView\":true,\"ObjectID\":4,\"ObjectNumber\":\"1985-52-14899\"}\n"
+        let sampleJSON = "{\"onView\":false,\"ObjectID\":4,\"ObjectNumber\":\"1985-52-14899\"}"
         let urlData = sampleJSON.data(using: .utf8)
         
         let servicePath = service.getLocalPathForURL(url: url!, repository: service.manualRequestRepository)
@@ -74,7 +74,61 @@ class CacheServiceTests: XCTestCase {
         
         XCTAssertEqual(expectedPath, servicePath)
         
-//        XCTAssertFalse(FileManager.default.fileExists(atPath: servicePath.path))
+        if FileManager.default.fileExists(atPath: servicePath.path) {
+            try! FileManager.default.removeItem(atPath: servicePath.path)
+        }
+        
+        XCTAssertFalse(FileManager.default.fileExists(atPath: servicePath.path))
+        
+        service.request(url: url!, uncached: false) { localPath, data in
+            
+            XCTAssertNotNil(localPath)
+            XCTAssertNotNil(data)
+            
+            XCTAssertEqual(expectedPath, localPath)
+            
+            XCTAssertEqual(urlData, data)
+            
+            let resultJSON = String(data: data!, encoding: .utf8)
+            
+            XCTAssertEqual(sampleJSON, resultJSON)
+            XCTAssertTrue(FileManager.default.fileExists(atPath: localPath!.path))
+            
+            expectationSuccess.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+    }
+    
+    func test_load_single_file_cached_with_file_available_locally() {
+        
+        let expectationSuccess = expectation(description: "Waiting for caching service to process request.")
+        
+        let service = CacheService()
+        
+        let url = URL(string: "http://localhost/api/v0/collection/objectIsOnView?query=4&api_token=WtPr21h94VqPLxEo8079vzg1ZcE6sIwYYcXur80EOULRB79zO0WBqdERk5hG")
+        
+        let sampleJSON = "{\"onView\":true,\"ObjectID\":4,\"ObjectNumber\":\"1985-52-14899\"}"
+        let urlData = sampleJSON.data(using: .utf8)
+        
+        let servicePath = service.getLocalPathForURL(url: url!, repository: service.manualRequestRepository)
+        let expectedPath = service.cacheURL
+            .appendingPathComponent(service.manualRequestRepository)
+            .appendingPathComponent("api")
+            .appendingPathComponent("v0")
+            .appendingPathComponent("collection")
+            .appendingPathComponent("objectIsOnView")
+        
+        XCTAssertEqual(expectedPath, servicePath)
+        
+        XCTAssertTrue(FileManager.default.fileExists(atPath: servicePath.path))
+        
+        try! sampleJSON.write(toFile: servicePath.path, atomically: true, encoding: .utf8)
         
         service.request(url: url!, uncached: false) { localPath, data in
             
